3855dcb19fcdedc0682c4f8be9ad3d26e12b5579
Arnoldo Thiago Monteiro Lima <arnoldo.thiago@gmail.com>
2018-04-13 19:13:30 -0300

    Atualização da função ADC_Init na biblioteca mspFunctions.h

--- MSP  Project/SMPS/Debug/main.d
+++ MSP  Project/SMPS/Debug/main.d
@@ -6,6 +6,7 @@ main.obj: C:/ti/ccsv8/ccs_base/msp430/include/msp430g2553.h
 main.obj: C:/ti/ccsv8/ccs_base/msp430/include/in430.h
 main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics.h
 main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: ../mspFunctions.h
 
 ../main.c: 
 C:/ti/ccsv8/ccs_base/msp430/include/msp430.h: 
@@ -13,3 +14,4 @@ C:/ti/ccsv8/ccs_base/msp430/include/msp430g2553.h:
 C:/ti/ccsv8/ccs_base/msp430/include/in430.h: 
 C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics.h: 
 C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics_legacy_undefs.h: 
+../mspFunctions.h: 
--- MSP  Project/SMPS/mspFunctions.h
+++ MSP  Project/SMPS/mspFunctions.h
@@ -8,34 +8,41 @@
 #ifndef MSPFUNCTIONS_H_
 #define MSPFUNCTIONS_H_
 
-#define IN_AD BIT1 | BIT4
-#define IN_AD_CH INCH_1
+/*5 channels of ADC*/
+#define ADC_CHANNELS 2
 
-void ADC_Init(void){
+/* Define the port to ADC*/
+#define AD_UP   BIT1
+#define AD_DOWN BIT4
+
+/* Initializing variable to store the ADCs*/
+unsigned int MESURES[ADC_CHANNELS] = { 0, 0 };
+
+void ADC_Init(void)
+{
     /* Configure the control register 0
      * SERF_X      - Select Reference.
      * ADC10SHT_X  - Sample and Hold Time.  This adjust how many clock cycles the sampling period will be.
      * ADC10ON     - ADC10 On.
+     * MSC         - Multiple sample and conversion.
      * */
-    ADC10CTL0 = SREF_0 + ADC10SHT_0 + ADC10ON;
+    ADC10CTL0 = SREF_0 + ADC10SHT_0 + ADC10ON + MSC;
 
     /* Configure the control register 1
      * IN_AD_CH    -
      * ADC10DIV_0  - Clock Divider.  Allows the chosen clock source used for the ADC to be divided down. (Divided by 1)
      * ADC10SSEL_3 - Clock Source Select. (SMCLK)
-     * CONSEQ_0    - Conversion Sequence Mode. (Single-channel-single-conversion)
+     * CONSEQ_3    - Conversion Sequence Mode. (Single-channel-single-conversion)
      * SHS_0       - Sample and Hold Source Select.
      * */
-    ADC10CTL1 = INCH_2 + ADC10DIV_0 + ADC10SSEL_3 + CONSEQ_0 + SHS_0;
+    ADC10CTL1 = INCH_1 + ADC10DIV_0 + ADC10SSEL_3 + CONSEQ_3 + SHS_0;
 
-    /*2 conversions*/
-    ADC10DTC1 = 0x02;
+    /*The number of transfers in each block*/
+    ADC10DTC1 = ADC_CHANNELS;
 
-    /*Disable digital I/O on P1.0 to P1.1*/
-    ADC10AE0 |= 0x02;
+    /*Choosing the port to ADC*/
+    ADC10AE0 = AD_UP + AD_DOWN;
 
-    /*Enable the input register 0*/
-    ADC10AE0 = BIT0 | BIT4;
 }
 
 #endif /* MSPFUNCTIONS_H_ */
