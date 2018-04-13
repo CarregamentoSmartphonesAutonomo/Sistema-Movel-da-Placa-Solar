5e8d53ef37bb1b7c3cef60e9a5a4808233597597
Arnoldo Thiago Monteiro Lima <arnoldo.thiago@gmail.com>
2018-04-13 17:14:08 -0300

    Adição da função Init_ADC

--- MSP  Project/SMPS/main.c
+++ MSP  Project/SMPS/main.c
@@ -1,12 +1,16 @@
+/* Contolador para seguidor solar e controlador dos motores da placa
+*/
+
 #include <msp430.h> 
+#include "mspFunctions.h"
 
+int main(void){
+    /*Turn off the watchdog timer*/
+	WDTCTL = WDTPW | WDTHOLD;
 
-/**
- * main.c
- */
-int main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;	// Turn off the watchdog timer
+	/*Setting the clock to 8Mhz*/
+	BCSCTL1 = CALBC1_8MHZ;
+	DCOCTL = CALDCO_8MHZ;
 	
 	return 0;
 }
